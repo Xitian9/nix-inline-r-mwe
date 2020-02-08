d26c89a7085fef622a02f7302a53af7ec2b8b7b9
Dennis Gosnell <cdep.illabout@gmail.com>
2020-02-08 22:10:17 +0900

    get building

--- default.nix
+++ default.nix
@@ -1,22 +1,33 @@
 let
-  config = {
-    packageOverrides = super: rec {
+  thisOverlay = self: super: {
+    my-R = self.rWrapper.override {
+      packages = with self.rPackages; [ dplyr ];
+    };
 
-      haskellPackages = super.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: {
-          nix-mwe = haskellPackagesNew.callPackage ./cabal.nix {
-            R = super.rWrapper.override {
-              packages = with super.rPackages; [ dplyr ];
-            };
-          };
-        };
+    haskellPackages = super.haskellPackages.override {
+      overrides = hself: hsuper: {
+        nix-mwe = self.haskell.lib.overrideCabal (hself.callCabal2nix "nix-mwe" ./. {}) (oldAttrs: {
+          buildTools = oldAttrs.buildTools or [] ++ [ self.makeWrapper ];
+          postInstall = oldAttrs.postInstall or "" + ''
+            wrapProgram $out/bin/nix-mwe --prefix 'PATH' ':' "${self.lib.getBin self.my-R}/bin"
+          '';
+        });
+
+        inline-r = self.haskell.lib.overrideCabal hsuper.inline-r (oldAttrs: {
+          buildDepends = (oldAttrs.buildDepends or []) ++ [ self.R ];
+        });
       };
-
     };
   };
 
-  pkgs = import <nixpkgs> { inherit config; };
+  nixpkgsSrc =
+    builtins.fetchTarball {
+      # nixpkgs master as of 2020-02-08.
+      url = "https://github.com/NixOS/nixpkgs/archive/4e95bba3d39360e405d567089d8e17fc9433fc69.tar.gz";
+      sha256 = "sha256:1xzilrxkf0jdd0ck2f2sbjxxplz73g2r0kpjyn5ka5l8frc1zlii";
+    };
 
+  pkgs = import nixpkgsSrc { overlays = [ thisOverlay ]; };
 in
 
-  pkgs.haskellPackages.nix-mwe
+pkgs.haskellPackages.nix-mwe
